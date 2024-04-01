194fb9d27cc7a9827ea0f2cda3c72e774e24dbdc
Yana <yana.rusnak.93@icloud.com>
2024-04-01 20:23:58 +0300

    add .gitignore

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int startBalance = 150;
+        int upBalance = 2_300;
+        int allBalance = startBalance + upBalance;
+
+        int bonus;
+        if (upBalance > 1000) {
+            bonus = upBalance / 100;
+        }
+        else {
+            bonus = 0;
+        }
+
+        System.out.println(bonus);
+        System.out.println(allBalance);
+
+    }
+}
